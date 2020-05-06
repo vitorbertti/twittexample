d9a759325ba764630ffd6c00fafbb95824674436
Vitor Bertti <berttivitor@gmail.com>
2020-05-06 19:13:02 -0300

    Added navbar

--- tweets/views.py
+++ tweets/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
+
+import random
+
 from .models import Tweet
 
+
 def home_view(request, *args, **kwargs):
-   return HttpResponse("<h1>Hey</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{'id': x.id, 'content': x.content,
+                    'likes': random.randint(0, 122)} for x in qs]
+    data = {
+        'isUser': False,
+        'response': tweets_list
+    }
+    return JsonResponse(data)
+
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
 
-   data = {
-      "id": tweet_id,
-   }
-   status = 200
-   try:
-      obj = Tweet.objects.get(id=tweet_id)
-      data['content'] = obj.content
-   except:
-      data['message'] = 'Not found'
-      status = 404
-      
-   return JsonResponse(data, status=status)
+    data = {
+        'id': tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = 'Not found'
+        status = 404
+
+    return JsonResponse(data, status=status)
