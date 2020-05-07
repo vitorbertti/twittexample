bbdbe4ef603ae0e9737f489a9248670746f8eec5
Vitor Bertti <berttivitor@gmail.com>
2020-05-07 15:00:06 -0300

    Added create tweet

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,20 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 
-import random
-
 from .models import Tweet
-
+from .forms import TweetForm
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={'form': form})
 
 
 def tweet_list_view(request, *args, **kwargs):
