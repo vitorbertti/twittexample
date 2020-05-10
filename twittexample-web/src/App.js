15cb77590df7f16c130bce6d4c303079cc91695f
Vitor Bertti <berttivitor@gmail.com>
2020-05-10 15:34:45 -0300

    Initialized frontend

---
+++ twittexample-web/src/App.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const [tweets, setTweets] = useState([]);
+  
+
+
+  useEffect(() => {
+
+    const myCallback = (response, status) => {
+      if(status === 200) {
+        setTweets(response);
+      }else {
+        alert('There was an error.');
+      }
+    };
+
+    loadTweets(myCallback);
+    
+  }, []);
+
+  function loadTweets (callback) {
+    const xhr = new XMLHttpRequest();
+    const method = 'GET';
+    const url = 'http://localhost:8000/api/tweets/';
+    const responseType = 'json';
+
+    xhr.responseType = responseType;
+    xhr.open(method, url);
+
+    xhr.onload = () => {
+      callback(xhr.response, xhr.status);
+    };
+    xhr.onerror = () => {
+      callback({'message': 'The request was an error'}, 400);
+    };
+    xhr.send();
+ }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index) => {
+            return <li>{tweet.content}</li>
+          })}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- twittexample/settings.py
+++ twittexample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'od%inzc!hk+tz(9ef0@_eocmukb7bu8=@*vud0x%(--+p9wqvg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 LOGIN_URL = '/login'
 MAX_TWEET_LENGTH = 240
 TWEET_ACTION_OPTIONS = ['like', 'unlike', 'retweet']
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third-party
     'rest_framework',
+    'corsheaders',
     # internal
     'tweets',
 ]
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
 
 DEFAULT_RENDERER_CLASSES = [
         'rest_framework.renderers.JSONRenderer',
