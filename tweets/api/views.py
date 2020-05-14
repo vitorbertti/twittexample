25925425d917062c27947ee7d55e80146e23342c
Vitor Bertti <berttivitor@gmail.com>
2020-05-14 20:32:59 -0300

    Created tweet feed view

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -86,63 +86,18 @@ def tweet_action_view(request, *args, **kwargs):
     return Response({}, status=200)
 
 
-
-# ---- PURE DJANGO
-
-
-def tweet_create_view_pure_django(request, *args, **kwargs):
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def tweet_feed_view(request, *args, **kwargs):
     user = request.user
-    if not request.user.is_authenticated:
-        user = None
-        if request.is_ajax():
-            return JsonResponse({}, status=401)
-        return redirect(settings.LOGIN_URL)
+    profiles = user.following.all()
+    followed_users_id = []
+    if profiles.exists():
+        followed_users_id = [x.user.id for x in profiles]
+    followed_users_id.append(user.id)
+    qs = Tweet.objects.filter(user__id__in=followed_users_id).order_by("-timestamp")
+    serializer = TweetSerializer(qs, many=True)
+    return Response( serializer.data, status=200)
 
-    form = TweetForm(request.POST or None)
-    next_url = request.POST.get('next') or None
-
-    if form.is_valid():
-        obj = form.save(commit=False)
-        obj.user = user
-        obj.save()
-        if request.is_ajax():
-            return JsonResponse(obj.serialize(), status=201)
-
-        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
-            return redirect(next_url)
-        form = TweetForm()
-
-    if form.errors:
-        if request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-            
-    return render(request, 'components/form.html', context={'form': form})
-
-
-def tweet_list_view_pure_django(request, *args, **kwargs):
-    qs = Tweet.objects.all()
-    tweets_list = [x.serialize() for x in qs]
-    data = {
-        'isUser': False,
-        'response': tweets_list
-    }
-    return JsonResponse(data)
-
-
-def tweet_detail_view_pure_django(request, tweet_id, *args, **kwargs):
-
-    data = {
-        'id': tweet_id,
-    }
-    status = 200
-    try:
-        obj = Tweet.objects.get(id=tweet_id)
-        data['content'] = obj.content
-    except:
-        data['message'] = 'Not found'
-        status = 404
-
-    return JsonResponse(data, status=status)
-
-def tweets_profile_view(request, username, *args, **kwargs):
-    return render(request, "tweets/profile.html", context={"profile_username": username})
+# def tweets_profile_view(request, username, *args, **kwargs):
+#     return render(request, "tweets/profile.html", context={"profile_username": username})
